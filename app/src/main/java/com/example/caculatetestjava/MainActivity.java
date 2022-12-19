9e09ca7642d61918f6ceb36362e928ea28f3240f
CHY <uouo911226@gmail.com>
2022-12-19 22:42:12 +0800

    layoutInitial

--- app/src/main/java/com/example/caculatetestjava/MainActivity.java
+++ app/src/main/java/com/example/caculatetestjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button btn = findViewById(R.id.AnsGameBtn);
-        Button btn2 = findViewById(R.id.SymGameBtn);
-        Button btn3 = findViewById(R.id.button);
+        ImageButton btn = findViewById(R.id.AnsGameBtn);
+        ImageButton btn2 = findViewById(R.id.SymGameBtn);
+        ImageButton btn3 = findViewById(R.id.button);
 
 
         btn.setOnClickListener(new View.OnClickListener() {
